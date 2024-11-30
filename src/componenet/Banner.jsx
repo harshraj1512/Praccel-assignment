1d49a6650dbe57c687f024d80a505eb5b0473994
Harsh Raj <harshprogram63@gmail.com>
2024-11-30 19:21:17 +0530

    some changes

--- src/componenet/Banner.jsx
+++ src/componenet/Banner.jsx
@@ -12,10 +12,10 @@ const Banner = () => {
         <div className='w-full md:w-1/2 mt-12 md:mt-32  '>
             <div className=" ml-24 mt-10">
             <h1 className="text-4xl font-bold mb-4">You <span className="text-blue-700">Read Aloud</span> App</h1>
-            <p className="text-xl">
+            <p className="text-xl font-medium">
               Get short reading passages in English regularly.
             </p>
-            <p className="text-xl">
+            <p className="text-xl font-medium">
               Read, get feedback, improve your reading.
             </p>
             </div>
--- src/componenet/Learn.jsx
+++ src/componenet/Learn.jsx
@@ -12,7 +12,7 @@ const Learn = () => {
         <div className='w-full md:w-1/2 mt-12 md:mt-32  '>
             <div className=" ml-24 mt-10">
             <h1 className="text-4xl font-bold mb-4">Get Feedback</h1>
-            <p className="text-xl">
+            <p className="text-xl font-medium">
             The app will highlight the <span className="bg-yellow-300 text-black p-1 rounded">mistakes</span> and give you a score
             </p>
             </div>
